305168041495c1d8b7ff059cb867cfd4796d4678
FrontendDeveloper <chygura.v@gmail.com>
2017-06-06 11:10:21 +0300

    added click area for arrows button

--- imcms/scripts/imcms_numberbox.js
+++ imcms/scripts/imcms_numberbox.js
@@ -20,7 +20,7 @@
         },
         deactivateNumberBox: function (e) {
             if (!$(e.target).closest(".imcms-number-box__input").length &&
-                e.target.classList[1] !== "imcms-number__button") {
+                e.target.classList[0] !== "imcms-number__button") {
 
                 $(".imcms-number-box__input").closest(".imcms-number-box")
                     .removeClass("imcms-number-box--active");
