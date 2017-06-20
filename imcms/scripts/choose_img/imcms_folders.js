f3080498e6519539b33bdae08bdf728844874984
FrontendDeveloper <chygura.v@gmail.com>
2017-06-20 15:15:47 +0300

    added folder urls

--- imcms/scripts/choose_img/imcms_folders.js
+++ imcms/scripts/choose_img/imcms_folders.js
@@ -6,6 +6,7 @@
     function getFoldersUrl() {
         return [
             "/srv/www/tomcat/instance/182/virt/webapp/WEB-INF/images",
+            "/srv/www/tomcat/instance/182/virt/webapp/WEB-INF/images/images_2019",
             "/srv/www/tomcat/instance/182/virt/webapp/WEB-INF/images/images_2018",
             "/srv/www/tomcat/instance/182/virt/webapp/WEB-INF/images/images_2018/flowers",
             "/srv/www/tomcat/instance/182/virt/webapp/WEB-INF/images/images_2018/flowers/XxX",
@@ -13,9 +14,12 @@
             "/srv/www/tomcat/instance/182/virt/webapp/WEB-INF/images/images_2017",
             "/srv/www/tomcat/instance/182/virt/webapp/WEB-INF/images/images_2017/cars",
             "/srv/www/tomcat/instance/182/virt/webapp/WEB-INF/images/images_2017/cars/bmw",
+            "/srv/www/tomcat/instance/182/virt/webapp/WEB-INF/images/images_2017/cars/lada",
+            "/srv/www/tomcat/instance/182/virt/webapp/WEB-INF/images/images_2017/cars/lada/kalyna",
             "/srv/www/tomcat/instance/182/virt/webapp/WEB-INF/images/images_2017/holiday",
             "/srv/www/tomcat/instance/182/virt/webapp/WEB-INF/images/images_2016",
             "/srv/www/tomcat/instance/182/virt/webapp/WEB-INF/images/images_2016/holiday",
+            "/srv/www/tomcat/instance/182/virt/webapp/WEB-INF/images/images_2016/spring",
             "/srv/www/tomcat/instance/182/virt/webapp/WEB-INF/images/images_2016/summer",
             "/srv/www/tomcat/instance/182/virt/webapp/WEB-INF/images/images_2016/summer/family",
             "/srv/www/tomcat/instance/182/virt/webapp/WEB-INF/images/images_2016/summer/family/porno",
