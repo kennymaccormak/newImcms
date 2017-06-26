31f5a99710140c2016f32d5c0e186ed36daa0786
FrontendDeveloper <chygura.v@gmail.com>
2017-06-26 13:17:55 +0300

    added css rules and scripts for images in "choose img" page

--- imcms/scripts/choose_img/imcms_choose_img.js
+++ imcms/scripts/choose_img/imcms_choose_img.js
@@ -1,7 +1,17 @@
 (function ( Imcms ) {
   Imcms.ChooseImg = {
     init: function () {
+      $(".imcms-choose-img-wrap").click(Imcms.ChooseImg.active);
+      $(".imcms-choose-img-description__button").click(Imcms.ChooseImg.removeImg);
+    },
+    active: function () {
+      var $img = $(this);
 
+      $img.toggleClass("imcms-choose-img-wrap--active");
+    },
+    removeImg:function () {
+      var $btn = $(this);
+      $btn.parents(".imcms-choose-img-wrap").remove();
     }
   };
 
