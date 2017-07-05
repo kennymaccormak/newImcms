713e884dd5a28987d8cc877d5c1b34a66ab6cdd5
FrontendDeveloper <chygura.v@gmail.com>
2017-07-05 14:12:10 +0300

    added logic: "cropImg" (demo);

--- imcms/scripts/image_editor/imcms_image_editor_crop.js
+++ imcms/scripts/image_editor/imcms_image_editor_crop.js
@@ -1,24 +1,35 @@
 (function ( Imcms ) {
   Imcms.ImageEditorCrop = {
     init: function () {
-      $( "#cropArea" ).mouseover( Imcms.ImageEditorCrop.moveCropArea );
-      $( "#angleTopLeft" ).mousedown( Imcms.ImageEditorCrop.resizeTopLeft );
-      $( "#angleTopRight" ).mousedown( Imcms.ImageEditorCrop.resizeTopRight );
-      $( "#angleBottomLeft" ).mousedown( Imcms.ImageEditorCrop.resizeBottomLeft );
-      $( "#angleBottomRight" ).mousedown( Imcms.ImageEditorCrop.resizeBottomRight );
-
+      $( "#cropArea" ).mouseover( Imcms.ImageEditorCrop.cropImg );
     },
-    moveCropArea: function () {
+    cropImg: function () {
       var cropArea     = $( "#cropArea" ),
           positionTop  = cropArea.css( "top" ),
           positionLeft = cropArea.css( "left" )
       ;
+
+      $( ".ui-icon-gripsmall-diagonal-se" ).css( {
+                                                   "bottom": "-5px",
+                                                   "right": "-5px",
+                                                   "opacity": 0
+                                                 } );
 
       cropArea.draggable( {
                             containment: "parent",
                             drag: function () {
                               positionTop = cropArea.css( "top" );
                               positionLeft = cropArea.css( "left" );
+                              cropArea.css( {
+                                              "background-position-x": "-" + positionLeft,
+                                              "background-position-y": "-" + positionTop
+                                            } );
+                            }
+                          } );
+
+      cropArea.resizable( {
+                            handles: "all",
+                            resize: function ( event, ui ) {
                               cropArea.css( {
                                               "background-position-x": "-" + positionLeft,
                                               "background-position-y": "-" + positionTop
